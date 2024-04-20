dcf89a572e2bc4c9c5206831d898cf3505edf79a
Bobby Newsom <bobby.newsom@gmail.com>
2024-04-20 14:54:59 -0500

    Initial commit

---
+++ src/main/java/bobbyromanenewsom/FamilyBlog/FamilyBlogApplication.java
@@ -0,0 +1,13 @@
+package bobbyromanenewsom.FamilyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBlogApplication.class, args);
+	}
+
+}
